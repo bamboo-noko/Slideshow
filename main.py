d3a6872ef801216c30118aee8ab67f8a91723253
bamboo-noko <plumhearts.yuki@gmail.com>
2019-02-04 17:54:29 +0900

    add interval

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ from kivy.uix.image import Image
 from kivy.clock import Clock
 from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import StringProperty, ObjectProperty
-from kivy.uix.popup import Popup
-from kivy.factory import Factory
 
 import os
 from random import randint
@@ -15,54 +13,85 @@ from glob import glob
 from os.path import join, dirname
 
 
-class LoadDialog(FloatLayout):
-    load = ObjectProperty(None)
-    cancel = ObjectProperty(None)
-
-
 class SlideshowController(FloatLayout):
     load_dir = ""
     files = []
     image_wid = ObjectProperty(None)
-    loadfile = ObjectProperty(None)
+    input_wid = ObjectProperty(None)
+    input_interval_wid = ObjectProperty(None)
+    input_repeat_wid = ObjectProperty(None)
+    label_current_time_wid = ObjectProperty(None)
+    label_current_num_wid = ObjectProperty(None)
+    event = ""
 
-    def dismiss_popup(self):
-        self._popup.dismiss()
+    isStart = False
 
-    def show_load(self):
-        content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
-        self._popup = Popup(title="Load directory", content=content,
-                            size_hint=(0.9, 0.9))
-        self._popup.open()
+    def callback(self, dt):
+        self.count_down()
+        if (int(self.label_current_time_wid.text) is not 0):
+            return
 
-    def change_image(self, dt):
+        self.label_current_num_wid.text = str(int(self.label_current_num_wid.text) + 1)
+        if(int(self.label_current_num_wid.text) > int(self.input_repeat_wid.text)):
+            self.end()
+            return
+
+        self.change_image()
+        self.init_time()
+
+    def count_down(self):
+        self.label_current_time_wid.text = str(int(self.label_current_time_wid.text) - 1)
+    
+    def init_time(self):
+        self.label_current_time_wid.text = self.input_interval_wid.text
+
+    def change_image(self):
         self.image_wid.source = self.files[randint(0, len(self.files)-1)]
         self.image_wid.reload()
 
     def start(self):
-        self.open_dir()
-        self.image_wid.source = self.files[randint(0, len(self.files)-1)]
-        self.image_wid.reload()
-        Clock.schedule_interval(self.change_image, 5)
+        if not self.validation_check():
+            return
 
-    def open_dir(self):
-        log = "load_dir:" + self.load_dir
-        Logger.info(log)
-        self.files = glob(join(self.load_dir, '*'))
+        isStart = True
+        self.files = glob(join(self.input_wid.text, '*'))
+        self.change_image()
+        self.init_time()
+        self.label_current_num_wid.text = "1"
+        self.event = Clock.schedule_interval(self.callback, 1)
 
-    def load(self, path, filename):
-        self.load_dir = path
-        self.dismiss_popup()
+        self.update_widget_state()
+        Logger.info("State:start")
+
+    def end(self):
+        self.image_wid.source = "./resources/imgs/black.png"
+        self.label_current_num_wid.text = "0"
+        self.event.cancel()
+        Logger.info("State:end")
+
+    def validation_check(self):
+        if not (os.path.exists(self.input_wid.text)):
+            return False
+
+        if not (os.path.isdir(self.input_wid.text)):
+            return False
+
+        if not (self.input_interval_wid.text.isdecimal()):
+            return False
+
+        if not (self.input_repeat_wid.text.isdecimal()):
+            return False
+
+        return True
+
+    def update_widget_state(self):
+        pass
 
 
 class SlideshowApp(App):
 
     def build(self):
         return SlideshowController()
-
-
-Factory.register('SlideshowController', cls=SlideshowController)
-Factory.register('LoadDialog', cls=LoadDialog)
 
 
 if __name__ == '__main__':
